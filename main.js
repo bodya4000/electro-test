4e863216af54d20003b91c488afe854134d79c17
Bohdan Ubuntu <b.shraier@adtelligent.com>
2025-11-12 11:13:40 +0200

    success turn on gpu

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow() {
-  const win = new BrowserWindow({
+// --- РОБОЧІ ФЛАГИ CHROMIUM ДЛЯ АПАРАТНОГО ПРИСКОРЕННЯ (VA-API) ---
+
+app.commandLine.appendSwitch(
+    'enable-features', 
+    'VaapiVideoDecoder,VaapiVideoDecodeLinuxGL,VaapiIgnoreDriverChecks'
+);
+
+app.commandLine.appendSwitch('use-gl', 'egl');
+
+app.commandLine.appendSwitch('ignore-gpu-blocklist');
+
+app.commandLine.appendSwitch('enable-zero-copy');
+
+app.commandLine.appendSwitch('disable-features', 'UseChromeOSDirectVideoDecoder');
+
+// -----------------------------------------------------------------
+
+
+function createWindows() {
+  // Вікно 1: твоя основна сторінка
+  const mainWin = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      experimentalFeatures: true
     }
   });
-  win.loadFile('index.html');
+  mainWin.loadFile('index.html');
+
+  // Вікно 2: сторінка GPU / перевірка
+  const gpuWin = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Спроба відкрити chrome://gpu/
+  gpuWin.loadURL('chrome://gpu').catch(() => {
+    // Якщо не працює, можна завантажити локальну сторінку з перевіркою WebGL/WebGPU
+  });
+
+  // Відкриваємо DevTools у обох вікнах (за бажанням)
+  mainWin.webContents.openDevTools();
+  gpuWin.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+	  console.log('GPU feature status:', app.getGPUFeatureStatus());
+	createWindows()
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindows();
+});
